3f088d501e0146e4167280946c0f40ba64c44771
Zeao <originsbcg@gmail.com>
2015-01-12 22:50:47 -0500

    Prettified Foxyform
    I think this works better.

---
+++ HC_Gaming/contact_us_form.php
@@ -0,0 +1,13 @@
+	(function(d){
+	  var l = d.getElementById("foxyform_embed_link_627000");
+	      i = d.createElement('IFRAME');
+	  i.src = "http://www.foxyform.com/form.php?id=627000&sec_hash=3a8b0d25e8e";
+	  i.width = "500px";
+	  i.height = "380px";
+	  i.frameborder = 0;
+	  i.setAttribute('style', 'border: none;');
+	  i.setAttribute('frameborder', '0');
+	  l.setAttribute('style', 'display: block; width: 350px; font-size: 0.7em; color: grey; text-align: right; text-decoration: none;');
+	  l.parentNode.insertBefore(i,l);
+	})(document);
+		
